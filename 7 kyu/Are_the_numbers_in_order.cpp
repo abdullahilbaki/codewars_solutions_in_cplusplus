2eca2a0c922e88fab62ac11f4bb7edf2b88e727d
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-17 02:50:32 +0600

    Update Are_the_numbers_in_order.cpp

--- 7 kyu/Are_the_numbers_in_order.cpp
+++ 7 kyu/Are_the_numbers_in_order.cpp
@@ -11,16 +11,16 @@ bool isAscOrder(std::vector<int> arr)
 int main()
 {
     // test cases
-    assert(isAscOrder({ 1, 2 }) == (true));
-    assert(isAscOrder({ 2, 1 }) == (false));
-    assert(isAscOrder({ 1, 2, 3 }) == (true));
+    assert(isAscOrder({ 1, 2 })    == (true)) ;
+    assert(isAscOrder({ 2, 1 })    == (false));
+    assert(isAscOrder({ 1, 2, 3 }) == (true)) ;
     assert(isAscOrder({ 1, 3, 2 }) == (false));
     assert(isAscOrder({ 2, 1, 3 }) == (false));
     assert(isAscOrder({ 2, 3, 1 }) == (false));
     assert(isAscOrder({ 3, 1, 2 }) == (false));
     assert(isAscOrder({ 3, 2, 1 }) == (false));
 
-    assert(isAscOrder({ 1, 4, 13, 97, 508, 1047, 20058 }) == (true));
+    assert(isAscOrder({ 1, 4, 13, 97, 508, 1047, 20058 })        == (true));
     assert(isAscOrder({ 56, 98, 123, 67, 742, 1024, 32, 90969 }) == (false));
 
     return 0;
