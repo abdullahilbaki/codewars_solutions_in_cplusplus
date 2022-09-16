e7da2841e14af0304fc7da0f57f3002b93491752
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-17 02:48:00 +0600

    Update Basic_Mathematical_Operations.cpp

--- 8 kyu/Basic_Mathematical_Operations.cpp
+++ 8 kyu/Basic_Mathematical_Operations.cpp
@@ -11,10 +11,10 @@ int basicOp(char op, int val1, int val2) {
 int main()
 {
     // basic tests
-    assert(basicOp('+', 5, 4) == (9));
-    assert(basicOp('-', 11, 8) == (3));
-    assert(basicOp('*', 3, 4) == (12));
-    assert(basicOp('/', 16, 4) == (4));
+    assert(basicOp('+', 5, 4)  == (9))  ;
+    assert(basicOp('-', 11, 8) == (3))  ;
+    assert(basicOp('*', 3, 4)  == (12)) ;
+    assert(basicOp('/', 16, 4) == (4))  ;
 
     return 0;
 
