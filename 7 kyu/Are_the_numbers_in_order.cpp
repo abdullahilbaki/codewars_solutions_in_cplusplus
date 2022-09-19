8b3f0c77112953b365dc3939d27b1910b371d7fa
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-19 19:52:33 +0600

    Update Are_the_numbers_in_order.cpp

--- 7 kyu/Are_the_numbers_in_order.cpp
+++ 7 kyu/Are_the_numbers_in_order.cpp
@@ -1,27 +1,39 @@
-#include <vector>
-#include <algorithm> // for is_sorted
-#include <cassert>   // for assert
+#include <igloo/igloo_alt.h>
+using namespace igloo;
 
 // solution
+#include <vector>
+#include <algorithm> // for is_sorted
+
 bool isAscOrder(std::vector<int> arr)
 {
     return is_sorted(begin(arr), end(arr));
 }
 
+
+// unit test
+Describe(ExampleTests)
+{
+    It(_0_BasicTests)
+    {
+        Assert::That(isAscOrder({ 1, 2 }), Equals(true));
+        Assert::That(isAscOrder({ 2, 1 }), Equals(false));
+        Assert::That(isAscOrder({ 1, 2, 3 }), Equals(true));
+        Assert::That(isAscOrder({ 1, 3, 2 }), Equals(false));
+        Assert::That(isAscOrder({ 2, 1, 3 }), Equals(false));
+        Assert::That(isAscOrder({ 2, 3, 1 }), Equals(false));
+        Assert::That(isAscOrder({ 3, 1, 2 }), Equals(false));
+        Assert::That(isAscOrder({ 3, 2, 1 }), Equals(false));
+    }
+
+    It(_1_AdvancedTests)
+    {
+        Assert::That(isAscOrder({ 1, 4, 13, 97, 508, 1047, 20058 }), Equals(true));
+        Assert::That(isAscOrder({ 56, 98, 123, 67, 742, 1024, 32, 90969 }), Equals(false));
+    }
+};
+
 int main()
 {
-    // test cases
-    assert(isAscOrder({ 1, 2 })    == (true)) ;
-    assert(isAscOrder({ 2, 1 })    == (false));
-    assert(isAscOrder({ 1, 2, 3 }) == (true)) ;
-    assert(isAscOrder({ 1, 3, 2 }) == (false));
-    assert(isAscOrder({ 2, 1, 3 }) == (false));
-    assert(isAscOrder({ 2, 3, 1 }) == (false));
-    assert(isAscOrder({ 3, 1, 2 }) == (false));
-    assert(isAscOrder({ 3, 2, 1 }) == (false));
-
-    assert(isAscOrder({ 1, 4, 13, 97, 508, 1047, 20058 })        == (true));
-    assert(isAscOrder({ 56, 98, 123, 67, 742, 1024, 32, 90969 }) == (false));
-
-    return 0;
+    return TestRunner::RunAllTests();
 }
