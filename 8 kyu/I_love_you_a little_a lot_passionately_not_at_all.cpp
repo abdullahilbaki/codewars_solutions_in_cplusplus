1f3b5ae434582b4e19812b9bb38407adb22d9269
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-19 19:23:59 +0600

    Create I_love_you_a little_a lot_passionately_not_at_all.cpp

---
+++ 8 kyu/I_love_you_a little_a lot_passionately_not_at_all.cpp
@@ -0,0 +1,28 @@
+#include <igloo/igloo_alt.h>
+using namespace igloo;
+
+#include <string>
+
+std::string how_much_i_love_you(int n) {
+    if (n % 6 == 1) return "I love you";
+    if (n % 6 == 2) return "a little";
+    if (n % 6 == 3) return "a lot";
+    if (n % 6 == 4) return "passionately";
+    if (n % 6 == 5) return "madly";
+    return "not at all";
+}
+
+Describe(decode)
+{
+    It(basic_tests)
+    {
+        Assert::That(how_much_i_love_you(7), Equals("I love you"));
+        Assert::That(how_much_i_love_you(3), Equals("a lot"));
+        Assert::That(how_much_i_love_you(6), Equals("not at all"));
+    }
+};
+
+int main()
+{
+    return TestRunner::RunAllTests();
+}
