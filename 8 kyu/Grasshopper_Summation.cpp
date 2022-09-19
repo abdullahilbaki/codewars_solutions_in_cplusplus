62333f0ae8fd62c7a28559cf7d7c12c3a2ddcfba
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-19 19:45:02 +0600

    Update Grasshopper_Summation.cpp

--- 8 kyu/Grasshopper_Summation.cpp
+++ 8 kyu/Grasshopper_Summation.cpp
@@ -1,15 +1,25 @@
-#include <cassert>
+#include <igloo/igloo_alt.h>
+using namespace igloo;
 
+// solution
 int summation(int num) {
     int sum{};
     for (int i{}; i <= num; i++) sum += i;
     return sum;
 }
 
+
+// unit test
+Describe(Test)
+{
+    It(Fixed__Tests)
+    {
+        Assert::That(summation(1), Equals(1));
+        Assert::That(summation(8), Equals(36));
+    }
+};
+
 int main()
 {
-    assert(summation(1) == (1));
-    assert(summation(8) == (36));
-
-    return 0;
+    return TestRunner::RunAllTests();
 }
