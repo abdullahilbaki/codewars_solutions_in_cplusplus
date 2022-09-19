0a0a92cddbe0068df2f3ec660174cf4e46c7b792
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-19 19:34:49 +0600

    Update Basic_Mathematical_Operations.cpp

--- 8 kyu/Basic_Mathematical_Operations.cpp
+++ 8 kyu/Basic_Mathematical_Operations.cpp
@@ -1,21 +1,26 @@
-#include <cassert> // for assert
+#include <igloo/igloo_alt.h>
+using namespace igloo;
 
 // solution
 int basicOp(char op, int val1, int val2) {
     return (op == '+') ? val1 + val2 :
-           (op == '-') ? val1 - val2 :
-           (op == '*') ? val1 * val2 :
-                         val1 / val2 ;
+        (op == '-') ? val1 - val2 :
+        (op == '*') ? val1 * val2 :
+        val1 / val2;
 }
+
+// unit test
+Describe(basic_op_samples)
+{
+    It(basic_tests) {
+        Assert::That(basicOp('+', 5, 4), Equals(9));
+        Assert::That(basicOp('-', 11, 8), Equals(3));
+        Assert::That(basicOp('*', 3, 4), Equals(12));
+        Assert::That(basicOp('/', 16, 4), Equals(4));
+    }
+};
 
 int main()
 {
-    // basic tests
-    assert(basicOp('+', 5, 4)  == (9))  ;
-    assert(basicOp('-', 11, 8) == (3))  ;
-    assert(basicOp('*', 3, 4)  == (12)) ;
-    assert(basicOp('/', 16, 4) == (4))  ;
-
-    return 0;
-
+    return TestRunner::RunAllTests();
 }
