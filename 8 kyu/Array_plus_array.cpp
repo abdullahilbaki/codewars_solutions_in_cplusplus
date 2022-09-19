36efdbdede85c16618dbd56ce6a6214405b9ab52
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-19 19:29:20 +0600

    Update Array_plus_array.cpp

--- 8 kyu/Array_plus_array.cpp
+++ 8 kyu/Array_plus_array.cpp
@@ -1,6 +1,8 @@
+#include <igloo/igloo_alt.h>
+using namespace igloo;
+
 #include <vector> 
 #include <numeric> // for std::accumulate
-#include <cassert>
 
 // solution
 int arrayPlusArray(std::vector<int> a, std::vector<int> b) {
@@ -8,14 +10,20 @@ int arrayPlusArray(std::vector<int> a, std::vector<int> b) {
     return std::accumulate(a.begin(), a.end(), 0);
 }
 
+// unit test
+using V = std::vector<int>;
+
+Describe(ArrayPlusArray) {
+    It(BasicTests) {
+        Assert::That(arrayPlusArray(V{ 1, 2, 3 }, V{ 4, 5, 6 }), Equals(21));
+        Assert::That(arrayPlusArray(V{ 1, -2, -3 }, V{ 4, -5, 6 }), Equals(1));
+        Assert::That(arrayPlusArray(V{ -1, -2, -3 }, V{ -4, -5, -6 }), Equals(-21));
+        Assert::That(arrayPlusArray(V{ 0, 0, 0 }, V{ 4, 5, 6 }), Equals(15));
+        Assert::That(arrayPlusArray(V{ -1 }, V{ -1 }), Equals(-2));
+    }
+};
+
 int main()
 {
-   // basic tests
-   assert(arrayPlusArray({ 1, 2, 3 }, { 4, 5, 6 }) == (21));
-   assert(arrayPlusArray({ 1, -2, -3 }, { 4, -5, 6 }) == (1));
-   assert(arrayPlusArray({ -1, -2, -3 }, { -4, -5, -6 }) == (-21));
-   assert(arrayPlusArray({ 0, 0, 0 }, { 4, 5, 6 }) == (15));
-   assert(arrayPlusArray({ -1 }, { -1 }) == (-2));
-
-   return 0;
+    return TestRunner::RunAllTests();
 }
