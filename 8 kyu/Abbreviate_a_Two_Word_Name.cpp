a732abef334f2bc5ef33a99a0c7d6924c1037e7d
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-09-21 01:50:11 +0600

    Create Abbreviate_a_Two_Word_Name.cpp

---
+++ 8 kyu/Abbreviate_a_Two_Word_Name.cpp
@@ -0,0 +1,39 @@
+#include <igloo/igloo_alt.h>
+using namespace igloo;
+
+// solution
+#include <string>
+#include <algorithm>
+
+std::string abbrevName(std::string name)
+{
+    std::string s = name;
+    std::transform(s.cbegin(), s.cend(), s.begin(), [](unsigned char c) { return std::toupper(c); });
+    std::string sc{};
+    for (size_t i{}; i < s.length(); i++) {
+        if (i == 0) sc.push_back(s[i]);
+        else if (s[i] == ' ') {
+            sc.push_back('.');
+            sc.push_back(s[i + 1]);
+        }
+    }
+    return sc;
+}
+
+// unit test
+Describe(Tests)
+{
+    It(Fixed_tests)
+    {
+        Assert::That(abbrevName("Sam Harris"), Equals("S.H"));
+        Assert::That(abbrevName("Patrick Feenan"), Equals("P.F"));
+        Assert::That(abbrevName("Evan Cole"), Equals("E.C"));
+        Assert::That(abbrevName("P Favuzzi"), Equals("P.F"));
+        Assert::That(abbrevName("David Mendieta"), Equals("D.M"));
+    }
+};
+
+int main()
+{
+    return TestRunner::RunAllTests();
+}
