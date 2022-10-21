31a5e8f1e02644b2b8054e419dcb7c3bbc3906b0
Abdullahil Baki <86188702+abdullahilbaki@users.noreply.github.com>
2022-10-21 14:00:10 +0600

    Create Convert_a_Number_to_a_String.cpp

---
+++ 8 kyu/Convert_a_Number_to_a_String.cpp
@@ -0,0 +1,23 @@
+#include <igloo/igloo_alt.h>
+using namespace igloo;
+
+// solution
+#include <string>
+auto number_to_string = [](int num) { return std::to_string(num); };
+
+
+// unit test
+Describe(number_to_string_function)
+{
+    It(should_convert_a_number_to_string)
+    {
+        Assert::That(number_to_string(1 + 2), Equals("3"));
+        Assert::That(number_to_string(67), Equals("67"));
+        Assert::That(number_to_string(-5), Equals("-5"));
+    }
+};
+
+int main()
+{
+    return TestRunner::RunAllTests();
+}
